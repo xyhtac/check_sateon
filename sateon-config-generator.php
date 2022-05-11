c895502a45573471a07e6e289bd88352339b70e4
xyhtac <73641145+xyhtac@users.noreply.github.com>
2022-05-11 16:01:49 +0200

    Add files via upload

---
+++ sateon-config-generator.php
@@ -0,0 +1,64 @@
+#!/usr/bin/php
+<?php
+# check_sateon Icinga Config Generator
+#
+# Generate Icinga for all sateon devices using cached device list.
+#
+# Run check_sateon.php once before executing this script.
+# Example: 
+# ./check_sateon.php --hostname 10.0.1.1 --status-dc dc-fault-list.txt --list-dc dc-list.txt --device DOOR-2.14
+
+$cfg['cache'] = "/tmp";
+$cfg['list-dc'] = "DC.txt";
+$cfg['status-dc'] = "dc-fault-list.txt";
+$cfg['config-file'] = "sateon-field-network.conf";
+$cfg['hostname'] = "10.0.0.1";
+$cfg['username'] = "spectator";
+$cfg['password'] = "secret_password";
+
+
+# reset parameters
+$n = 0;
+$i = 0;
+$cachedata = "";
+$config = "";
+
+
+
+# compose cache url
+$deviceIDcache = $cfg['cache']."/".$cfg['list-dc'];
+
+# read data from cache file and split it into lines
+$cachedata = explode("\n", file_get_contents($deviceIDcache));
+
+# iterate through lines
+foreach ($cachedata as &$line) {
+	# check if cache line is parsable
+	if ( preg_match("/\=/", $line) ) {
+		# read line into variable pairs
+		list($idstr, $dcname) = explode("=", $line);
+		
+		$config_line = <<<EOL
+object Host "$dcname" {
+	check_command = "check_sateon"
+	vars.hostname = "$cfg['hostname']"
+	vars.username = "$cfg['username']"
+	vars.password = "$cfg['password']"
+	vars.status_dc = "$cfg['status-dc']"
+	vars.list_dc = "$cfg['list-dc']"
+	vars.type = "Controller"
+	vars.description = "SATEON panel $dcname"
+}
+
+EOL;
+		$config = $config.$config_line;
+	}
+}
+
+# compose config url
+$configname = $cfg['cache']."/".$cfg['config-file'];
+
+file_put_contents($configname, $config);
+
+
+?>
